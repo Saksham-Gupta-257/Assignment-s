ba602bd27b264e0330644455ea2bed11dff66c93
Saksham-Gupta-257 <saksham.gupta.2507@gmail.com>
2025-03-14 17:49:26 +0530

    Update 3) Implement a program to find the factorial of a given number.java

--- Java Assignment 1/Basic Java/3) Implement a program to find the factorial of a given number.java
+++ Java Assignment 1/Basic Java/3) Implement a program to find the factorial of a given number.java
@@ -13,6 +13,7 @@ public class Factorial{
             return n * factorial(n - 1);
         }
     }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
