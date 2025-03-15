6bc140efc3f251c8885f7722165453764672ca69
Saksham-Gupta-257 <saksham.gupta.2507@gmail.com>
2025-03-15 21:22:35 +0530

    Create 1) Write a program to reverse a given string.java

---
+++ Java Assignment 1/String Manipulation/1) Write a program to reverse a given string.java
@@ -0,0 +1,26 @@
+// 1) Write a program to reverse a given string.
+
+// Code:
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+        
+        scanner.close();
+    }
+}
+
+
+// Output:
+// Enter a string: assignment
+// Reversed string: tnemngissa
