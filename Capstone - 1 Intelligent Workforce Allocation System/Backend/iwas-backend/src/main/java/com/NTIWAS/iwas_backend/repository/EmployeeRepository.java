c8e118861914a79155af9830837efe6b7e8946d7
Saksham-Gupta-257 <saksham.gupta.2507@gmail.com>
2025-03-18 22:01:58 +0530

    capstone -1 folder delete

--- Capstone - 1 Intelligent Workforce Allocation System/Backend/iwas-backend/src/main/java/com/NTIWAS/iwas_backend/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.NTIWAS.iwas_backend.repository;
-
-import com.NTIWAS.iwas_backend.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
--- Capstone - 1 Intelligent Workforce Allocation System/Backend/iwas-backend/src/main/java/com/NTIWAS/iwas_backend/repository/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.NTIWAS.iwas_backend.repository;
-
-import com.NTIWAS.iwas_backend.model.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends JpaRepository<Skill, Integer> {
-}
